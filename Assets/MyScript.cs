45502783e64792d62b0723ccd02de1c37bd6d336
Gabriel Hajduk <gabrielh.hajduk2@gmail.com>
2019-05-05 21:33:15 +0200

    Working verion

---
+++ Assets/MyScript.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class MyScript : MonoBehaviour
+{
+    public Transform camera;
+    public GameObject[] markers;
+    public GameObject myObject;
+    DefaultTrackableEventHandler[] dt;
+    GameObject trackingObject;
+    Renderer rendererComponents;
+    Collider colliderComponents;
+    bool awaken;
+    // Canvas canvasComponents;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        awaken = false;
+        int i = 0;
+        dt = new DefaultTrackableEventHandler[markers.Length];
+        foreach(GameObject obj in markers)
+        {
+            dt[i] = obj.GetComponent<DefaultTrackableEventHandler>();
+            i++;
+        }
+        Vector3 vectorObj = positionOfObject(markers[0].transform.position, markers[1].transform.position, markers[2].transform.position);
+        Quaternion qua = new Quaternion(0, 0, 0, 0);
+        trackingObject = Instantiate(myObject, vectorObj, qua);
+        
+        rendererComponents = trackingObject.GetComponent<Renderer>();
+        colliderComponents = trackingObject.GetComponent<Collider>();
+        if (rendererComponents != null)
+            rendererComponents.enabled = false;
+        if (colliderComponents != null)
+            colliderComponents.enabled = false;
+        trackingObject.SetActive(false);
+
+        var childrenRenderer = trackingObject.GetComponentsInChildren<Renderer>(true);
+        var childrenCollider = trackingObject.GetComponentsInChildren<Collider>(true);
+        foreach (var component in childrenRenderer)
+            component.enabled = false;
+        foreach (var components in childrenCollider)
+            components.enabled = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isDetected(dt[0].detected, dt[1].detected, dt[2].detected))
+        {
+            trackingObject.SetActive(true);
+            if (rendererComponents != null)
+                rendererComponents.enabled = true;
+            if (colliderComponents != null)
+                colliderComponents.enabled = true;
+            Vector3 vectorObj = positionOfObject(markers[0].transform.position, markers[1].transform.position, markers[2].transform.position);
+            trackingObject.transform.position = vectorObj;
+            //trackingObject.transform.LookAt(camera);
+            Debug.Log("Moja pozycja: " + trackingObject.transform.position);
+            var childrenRenderer = trackingObject.GetComponentsInChildren<Renderer>(true);
+            var childrenCollider = trackingObject.GetComponentsInChildren<Collider>(true);
+            foreach (var component in childrenRenderer)
+                component.enabled = true;
+            foreach (var components in childrenCollider)
+                components.enabled = true;
+            var AudioChild = trackingObject.GetComponentInChildren<AudioSource>(true);
+            if (!AudioChild.isPlaying && !awaken)
+                AudioChild.Play();
+            awaken = true;
+        }
+        else
+        {
+            var AudioChild = trackingObject.GetComponentInChildren<AudioSource>(true);
+            if (AudioChild.isPlaying && !awaken)
+                AudioChild.Pause();
+            awaken = false;
+            var childrenRenderer = trackingObject.GetComponentsInChildren<Renderer>(true);
+            var childrenCollider = trackingObject.GetComponentsInChildren<Collider>(true);
+            foreach (var component in childrenRenderer)
+                component.enabled = false;
+            foreach (var components in childrenCollider)
+                components.enabled = false;
+            if (rendererComponents != null)
+                rendererComponents.enabled = false;
+            if (colliderComponents != null)
+                colliderComponents.enabled = false;
+            trackingObject.SetActive(false);
+        }
+    }
+
+    bool isDetected(bool one, bool two, bool three)
+    {
+        if (one && two && three)
+            return true;
+        else
+            return false;
+    }
+
+    Vector3 positionOfObject(Vector3 one, Vector3 two, Vector3 three)
+    {
+        float minX = 1000f, maxX = -1000f, minY = 1000f, maxY = -1000f, minZ = 1000f, maxZ = -1000f, x, y, z;
+        minX = minX > one.x ? one.x : minX;
+        minX = minX > two.x ? two.x : minX;
+        minX = minX > three.x ? three.x : minX;
+
+        minY = minY > one.y ? one.y : minY;
+        minY = minY > two.y ? two.y : minY;
+        minY = minY > three.y ? three.y : minY;
+
+        minZ = minZ > one.z ? one.z : minZ;
+        minZ = minZ > two.z ? two.z : minZ;
+        minZ = minZ > three.z ? three.z : minZ;
+
+        maxX = maxX < one.x ? one.x : maxX;
+        maxX = maxX < two.x ? two.x : maxX;
+        maxX = maxX < three.x ? three.x : maxX;
+
+        maxY = maxY < one.y ? one.y : maxY;
+        maxY = maxY < two.y ? two.y : maxY;
+        maxY = maxY < three.y ? three.y : maxY;
+
+        maxZ = maxZ < one.z ? one.z : maxZ;
+        maxZ = maxZ < two.z ? two.z : maxZ;
+        maxZ = maxZ < three.z ? three.z : maxZ;
+        x = (minX + maxX) / 2;
+        y = (minY + maxY) / 2;
+        z = (minZ + maxZ) / 2;
+        Vector3 returningVector = new Vector3(x, y, z);
+        return returningVector;
+    }
+}
---
+++ Assets/MyScriptOrVer.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class MyScriptOrVer : MonoBehaviour
+{
+    public GameObject[] markers;
+    public GameObject[] displayObjects;
+    private DefaultTrackableEventHandler[] dteh;
+    private Renderer[] rendererComponents;
+    private Collider[] colliderComponents;
+    void Awake()
+    {
+        int i = 0;
+        dteh = new DefaultTrackableEventHandler[markers.Length];
+        foreach (GameObject obj in markers)
+        {
+            dteh[i] = obj.GetComponent<DefaultTrackableEventHandler>();
+            i++;
+        }
+        rendererComponents = new Renderer[markers.Length];
+        colliderComponents = new Collider[markers.Length];
+        for(int zxc = 0; zxc<3; zxc++)
+        {
+            rendererComponents[zxc] = displayObjects[zxc].GetComponent<Renderer>();
+            colliderComponents[zxc] = displayObjects[zxc].GetComponent<Collider>();
+            if (rendererComponents[zxc] != null)
+                rendererComponents[zxc].enabled = false;
+            if (colliderComponents[zxc] != null)
+                colliderComponents[zxc].enabled = false;
+            displayObjects[zxc].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dteh[0].detected)
+        {
+            if (rendererComponents[0] != null)
+                rendererComponents[0].enabled = true;
+            if (colliderComponents[0] != null)
+                colliderComponents[0].enabled = true;
+            displayObjects[0].SetActive(true);
+
+            if (rendererComponents[1] != null)
+                rendererComponents[1].enabled = false;
+            if (colliderComponents[1] != null)
+                colliderComponents[1].enabled = false;
+            displayObjects[1].SetActive(false);
+
+            if (rendererComponents[2] != null)
+                rendererComponents[2].enabled = false;
+            if (colliderComponents[2] != null)
+                colliderComponents[2].enabled = false;
+            displayObjects[2].SetActive(false);
+        }
+        if (dteh[1].detected)
+        {
+            if (rendererComponents[1] != null)
+                rendererComponents[1].enabled = true;
+            if (colliderComponents[1] != null)
+                colliderComponents[1].enabled = true;
+            displayObjects[1].SetActive(true);
+
+            if (rendererComponents[2] != null)
+                rendererComponents[2].enabled = false;
+            if (colliderComponents[2] != null)
+                colliderComponents[2].enabled = false;
+            displayObjects[2].SetActive(false);
+
+            if (rendererComponents[0] != null)
+                rendererComponents[0].enabled = false;
+            if (colliderComponents[0] != null)
+                colliderComponents[0].enabled = false;
+            displayObjects[0].SetActive(false);
+        }
+        if (dteh[2].detected)
+        {
+            if (rendererComponents[2] != null)
+                rendererComponents[2].enabled = true;
+            if (colliderComponents[2] != null)
+                colliderComponents[2].enabled = true;
+            displayObjects[2].SetActive(true);
+
+            if (rendererComponents[1] != null)
+                rendererComponents[1].enabled = false;
+            if (colliderComponents[1] != null)
+                colliderComponents[1].enabled = false;
+            displayObjects[1].SetActive(false);
+
+            if (rendererComponents[0] != null)
+                rendererComponents[0].enabled = false;
+            if (colliderComponents[0] != null)
+                colliderComponents[0].enabled = false;
+            displayObjects[0].SetActive(false);
+        }
+       /* else
+        {
+            if (rendererComponents[2] != null)
+                rendererComponents[2].enabled = false;
+            if (colliderComponents[2] != null)
+                colliderComponents[2].enabled = false;
+            displayObjects[2].SetActive(false);
+
+            if (rendererComponents[1] != null)
+                rendererComponents[1].enabled = false;
+            if (colliderComponents[1] != null)
+                colliderComponents[1].enabled = false;
+            displayObjects[1].SetActive(false);
+
+            if (rendererComponents[0] != null)
+                rendererComponents[0].enabled = false;
+            if (colliderComponents[0] != null)
+                colliderComponents[0].enabled = false;
+            displayObjects[0].SetActive(false);
+        }
+        */
+    }
+}
